9b1d359889ac035fb561a1c0c9b15240043985c4
P3lina <purschwitzplay@gmail.com>
2022-04-11 14:38:11 +0200

    weather changed

--- src/app/_template/template-widget/template-widget-weather/template-widget-weather.component.ts
+++ src/app/_template/template-widget/template-widget-weather/template-widget-weather.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
 import {  WeatherWidget  } from '../../../_interface/weather-widget';
+import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { analyzeAndValidateNgModules } from '@angular/compiler';
-import {CdkDragRelease, DragRef} from "@angular/cdk/drag-drop";
+import {CdkDragMove, CdkDragRelease, DragRef} from "@angular/cdk/drag-drop";
+
 @Component({
   selector: 'app-template-widget-weather',
   templateUrl: './template-widget-weather.component.html',
@@ -10,17 +11,23 @@ import {CdkDragRelease, DragRef} from "@angular/cdk/drag-drop";
 })
 export class TemplateWidgetWeatherComponent implements OnInit {
 
-  dragPosition = {x:0,y:0};
+
+
+
+
+  dragPosition = {x: 0, y: 0};
   releasedObject(event: CdkDragRelease<DragRef>): void {
-    let position = event.source.getRootElement().getAttribute("style");
-    // @ts-ignore
-    let positionX = Number(position.split("translate3d(")[1].split("px")[0])
-    positionX = positionX%10<5?positionX-positionX%10:positionX+10-positionX%10
-    // @ts-ignore
-    let positionY = Number(position.split("translate3d(")[1].split("px")[1].split(", ")[1])
-    positionY = positionY%10<5?positionY-positionY%10:positionY+10-positionY%10
+    if(!this.altpressed) {
+      let position = event.source.getRootElement().getAttribute("style");
+      // @ts-ignore
+      let positionX = Number(position.split("translate3d(")[1].split("px")[0])
+      positionX = positionX % 10 < 5 ? positionX - positionX % 10 : positionX + 10 - positionX % 10
+      // @ts-ignore
+      let positionY = Number(position.split("translate3d(")[1].split("px")[1].split(", ")[1])
+      positionY = positionY % 10 < 5 ? positionY - positionY % 10 : positionY + 10 - positionY % 10
       //transform: translate3d(252px, 248px, 0px);
-    this.dragPosition = {x: positionX, y: positionY}
+      this.dragPosition = {x: positionX, y: positionY}
+    }
   }
 
   //Add ng lint
@@ -34,37 +41,45 @@ export class TemplateWidgetWeatherComponent implements OnInit {
     };
   }
 
+  altpressed = false;
   ngOnInit(): void {
+    document.addEventListener("keydown", e => {
+    if (e.keyCode === 18)
+      this.altpressed = true;
+    });
+    document.addEventListener("keyup", e => {
+      if (e.keyCode === 18)
+        this.altpressed = false;
+    });
     this.getMyLocation();
   }
 
   public getWeather(url: string): void {
     this.http.get<any>(url).subscribe(res => {
       var test = this.WeatherWidget$;
-      if(res.weather[0].main == 'Clouds')
-      {
-        test.weather='wolkig'
+      if (res.weather[0].main == 'Clouds') {
+        test.weather = 'wolkig'
       }
-      test.temperature = Number((res.main.temp -273.15).toFixed(0));
+      test.temperature = Number((res.main.temp - 273.15).toFixed(0));
     });
   }
 
   public getMyLocation(): void {
-    navigator.geolocation.getCurrentPosition(this.successCallback,this.errorCallback);
+    navigator.geolocation.getCurrentPosition(this.successCallback, this.errorCallback);
   }
 
-  successCallback = (position:any) =>{
+  successCallback = (position: any) => {
     var x = position.coords.latitude;
     var y = position.coords.longitude;
-    this.getWeather("http://api.openweathermap.org/data/2.5/weather?lat="+x+"&lon="+y+"&appid=332468f7d43cc6c6858ca81118204cdd")
+    this.getWeather("http://api.openweathermap.org/data/2.5/weather?lat=" + x + "&lon=" + y + "&appid=332468f7d43cc6c6858ca81118204cdd")
     //this.getAddress(x, y).then(console.log)
-    this.displayLocation(x,y)
+    this.displayLocation(x, y)
   };
 
 
-  errorCallback = function(error:any){
+  errorCallback = function (error: any) {
     var errorMessage = 'Unknown error';
-    switch(error.code) {
+    switch (error.code) {
       case 1:
         errorMessage = 'Permission denied';
         break;
@@ -79,7 +94,7 @@ export class TemplateWidgetWeatherComponent implements OnInit {
   };
 
 
-  displayLocation(latitude:any,longitude:any):any{
+  displayLocation(latitude: any, longitude: any): any {
     var request = new XMLHttpRequest();
 
     var method = 'GET';
@@ -87,8 +102,8 @@ export class TemplateWidgetWeatherComponent implements OnInit {
     var url = 'https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyAT3XtwPNxF9rcPaEB1HGHpSRRliPZQwSA&latlng=' + latitude + ',' + longitude + '&sensor=true';
     var async = true;
     request.open(method, url, async);
-    request.onreadystatechange = function(){
-      if(request.readyState == 4 && request.status == 200){
+    request.onreadystatechange = function () {
+      if (request.readyState == 4 && request.status == 200) {
         var data = JSON.parse(request.responseText);
         var address = data.results[0];
         //console.log(address.address_components)
@@ -105,6 +120,7 @@ export class TemplateWidgetWeatherComponent implements OnInit {
 
           var method = 'GET';
           var url = 'https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyAT3XtwPNxF9rcPaEB1HGHpSRRliPZQwSA&latlng=' + latitude + ',' + longitude + '&sensor=true';
+
           var async = true;
 
           request.open(method, url, async);
