b147a28802e97707d18b4646aa55566a614595d3
Crixos86 <71458623+Crixos86@users.noreply.github.com>
2022-04-18 16:13:14 +0200

    New Dashboard for Drag and Drop

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,33 @@
-import { Component } from '@angular/core';
-import {
-  CdkDrag,
-  CdkDragStart,
-  CdkDropList,
-  CdkDropListGroup,
-  CdkDragMove,
-  CdkDragEnter,
-  moveItemInArray
-} from "@angular/cdk/drag-drop";
+import { Component, OnInit } from '@angular/core';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Papatohu';
+export class AppComponent implements OnInit {
+  title = 'DashboardV2';
+  public list = [];
+
+  public ngOnInit() {
+    this.list = [
+      { rows: 2, cols: 2, data: 1 },
+      { rows: 1, cols: 1, data: 2 },
+      { rows: 2, cols: 1, data: 3 },
+      { rows: 1, cols: 1, data: 4 },
+
+      { rows: 1, cols: 1, data: 5 },
+      { rows: 2, cols: 2, data: 6 },
+      { rows: 1, cols: 1, data: 7 },
+      { rows: 1, cols: 1, data: 8 },
+      { rows: 1, cols: 1, data: 9 },
+
+      { rows: 1, cols: 1, data: 10 },
+      { rows: 1, cols: 1, data: 11 },
+      { rows: 2, cols: 2, data: 12 },
+      { rows: 1, cols: 1, data: 13 },
+      { rows: 1, cols: 1, data: 14 },
+
+    ];
+  }
 }
