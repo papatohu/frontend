1d1f4ddea7b437c98ae9f564ca1935c4861b5059
P3lina <purschwitzplay@gmail.com>
2022-04-22 13:57:46 +0200

    x,y Poitionen der draggable Widgets ausgelagert

--- src/app/widgets/widgets.component.ts
+++ src/app/widgets/widgets.component.ts
@@ -11,14 +11,24 @@ export class WidgetsComponent {
     console.log('on layout updated', layout);
     this.layout = layout;
   }
-
+  backendCall() {
+    //set widgetPositions Object
+  }
+  private widgetPositions = {
+    "weather": { "x": 0, "y": 0 },
+    "nasa": { "x": 0, "y": 1 },
+    "cartoon": { "x": 0, "y": 2 }
+  }
   cols: number = 6;
   rowHeight: number = 100;
   layout: KtdGridLayout = [
-    {id: '0', x: 0, y: 0, w: 1, h: 2},
-    {id: '1', x: 3, y: 0, w: 2, h: 3},
-    {id: '2', x: 0, y: 3, w: 1, h: 3, minW: 2, minH: 3},
-    {id: '3', x: 3, y: 3, w: 3, h: 1, minW: 2, maxW: 3, minH: 2, maxH: 5},
+    //{id: '0', x: 0, y: 0, w: 1, h: 2},
+    //Weather-Widget
+    {id: '1', x: this.widgetPositions.weather.x, y: this.widgetPositions.weather.y, w: 1, h: 1},
+    //Nasa-Widget
+    {id: '2', x: this.widgetPositions.nasa.x, y: this.widgetPositions.nasa.y, w: 1, h: 1, minW: 2, minH: 3},
+    //Cartoon-Widget
+    {id: '3', x: this.widgetPositions.cartoon.x, y: this.widgetPositions.cartoon.y, w: 1, h: 1, minW: 2, maxW: 3, minH: 2, maxH: 5},
   ];
   trackById = ktdTrackById
 }
